d655a070df0024e1ed94aff95598f6a86a8657d2
Oleg Zasedatelev <ol.zasedatelew@yandex.ru>
2022-05-03 21:44:48 +0300

    изменеия добавлены

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,36 @@
+const dropList = document.querySelector(".dropdown__list");
+    listItem = document.querySelectorAll('.dropdown__item');
+let listValue = document.querySelector('.dropdown__value');
+    
+
+
+function openList() {
+    dropList.classList.add('dropdown__list_active');
+}
+
+function titleChange(){
+    
+    listItem.forEach(item =>{
+        item.addEventListener("click", () =>{
+            listValue.textContent = item.textContent;
+            });
+    });
+    dropList.className = 'dropdown__list';   
+}
+
+
+
+
+
+function main(){
+
+    listValue.addEventListener("click", openList);
+    titleChange();
+}
+
+main();
+
+
+    
+
+
